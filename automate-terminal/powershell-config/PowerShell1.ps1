c926d1f4bcd31ad7dc185256e529b52ca8b2205d
imranh37 <hmimran.in@gmail.com>
2023-07-07 19:03:33 +0600

    automate terminal fonts added

--- automate-terminal/powershell-config/PowerShell1.ps1
+++ automate-terminal/powershell-config/PowerShell1.ps1
@@ -23,12 +23,4 @@ Install-Module -Name PowerShellGet -Force
 $scriptPath = ".\PowerShell2.ps1"
 
 Start-Process powershell.exe -ArgumentList "-NoExit", "-Command", "$scriptPath" -Verb RunAs
-
-# PSReadLine Module
-Install-Module PSReadLine -AllowPrerelease -Force
-
-# Copying Files
-# New-Item "C:\Users\$Env:UserName\Documents\WindowsPowerShell\" -ItemType Directory
-Copy-Item -Path "$pwd\*" -Destination "C:\Users\$Env:UserName\Documents\WindowsPowerShell" -Recurse
-Remove-Item "C:\Users\$Env:UserName\Documents\WindowsPowerShell\Instruction.txt"
-Remove-Item "C:\Users\$Env:UserName\Documents\WindowsPowerShell\PowerShell.ps1"
+Exit
--- automate-terminal/powershell-config/PowerShell2.ps1
+++ automate-terminal/powershell-config/PowerShell2.ps1
@@ -7,3 +7,9 @@ Copy-Item -Path "$pwd\*" -Destination "C:\Users\$Env:UserName\Documents\WindowsP
 Remove-Item "C:\Users\$Env:UserName\Documents\WindowsPowerShell\Instruction.txt"
 Remove-Item "C:\Users\$Env:UserName\Documents\WindowsPowerShell\PowerShell1.ps1"
 Remove-Item "C:\Users\$Env:UserName\Documents\WindowsPowerShell\PowerShell2.ps1"
+
+# font installing
+$scriptPath = ".\fonts\fonts.ps1"
+
+Start-Process powershell.exe -ArgumentList "-NoExit", "-Command", "$scriptPath" -Verb RunAs
+Exit
