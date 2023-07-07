fb58107adaf44b1b702185b1de31ed7bd0a02bb8
imranh37 <hmimran.in@gmail.com>
2023-07-07 18:26:18 +0600

    automate terminal update 6

---
+++ automate-terminal/powershell-config/PowerShell1.ps1
@@ -0,0 +1,40 @@
+# Ask for elevated permissions if required
+If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
+	Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+	Exit
+}
+
+# oh-my-posh installation
+winget install JanDeDobbeleer.OhMyPosh -s winget
+
+# Making Profile for Posh
+$PROFILE
+
+# Set-Execution Policy
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
+
+#Terminal Icons Module
+Install-Module -Name Terminal-Icons -Repository PSGallery -Force
+
+# PowerShellGet Module
+Install-Module -Name PowerShellGet -Force
+
+#Following Scripts
+$scriptPath = ".\PowerShell2.ps1"
+
+Start-Process powershell.exe -ArgumentList "-NoExit", "-Command", "& '$scriptPath'" -Verb RunAs
+
+
+
+
+# Restart PowerShell as administrator
+Start-Process powershell.exe -Verb RunAs -ArgumentList "-NoExit","-Command ""& { $script = '$($MyInvocation.MyCommand.Path)'; Start-Process powershell.exe -ArgumentList '-NoExit','-Command',' & ''$script'' ' -Verb RunAs }"""
+
+# PSReadLine Module
+Install-Module PSReadLine -AllowPrerelease -Force
+
+# Copying Files
+# New-Item "C:\Users\$Env:UserName\Documents\WindowsPowerShell\" -ItemType Directory
+Copy-Item -Path "$pwd\*" -Destination "C:\Users\$Env:UserName\Documents\WindowsPowerShell" -Recurse
+Remove-Item "C:\Users\$Env:UserName\Documents\WindowsPowerShell\Instruction.txt"
+Remove-Item "C:\Users\$Env:UserName\Documents\WindowsPowerShell\PowerShell.ps1"
---
+++ automate-terminal/powershell-config/PowerShell2.ps1
@@ -0,0 +1,9 @@
+# PSReadLine Module
+Install-Module PSReadLine -AllowPrerelease -Force
+
+# Copying Files
+# New-Item "C:\Users\$Env:UserName\Documents\WindowsPowerShell\" -ItemType Directory
+Copy-Item -Path "$pwd\*" -Destination "C:\Users\$Env:UserName\Documents\WindowsPowerShell" -Recurse
+Remove-Item "C:\Users\$Env:UserName\Documents\WindowsPowerShell\Instruction.txt"
+Remove-Item "C:\Users\$Env:UserName\Documents\WindowsPowerShell\PowerShell1.ps1"
+Remove-Item "C:\Users\$Env:UserName\Documents\WindowsPowerShell\PowerShell2.ps1"
