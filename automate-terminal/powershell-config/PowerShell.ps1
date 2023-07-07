ccb750cea037902b6e7e656a7664544781d0fce9
imranh37 <hmimran.in@gmail.com>
2023-07-07 17:46:27 +0600

    automate terminal update 2

--- automate-terminal/powershell-config/PowerShell.ps1
+++ automate-terminal/powershell-config/PowerShell.ps1
@@ -13,11 +13,15 @@ $PROFILE
 # Set-Execution Policy
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
 
-#Terminal Icons Installation
-Install-Module -Name Terminal-Icons -Repository PSGallery
+#Terminal Icons Module
+Install-Module -Name Terminal-Icons -Repository PSGallery -Force
+
+# PSReadLine Module
+Install-Module -Name PowerShellNuGet -Force
+Install-Module PSReadLine -AllowPrerelease -Force
 
 # Copying Files
-New-Item "C:\Users\$Env:UserName\Documents\WindowsPowerShell\" -ItemType Directory
+# New-Item "C:\Users\$Env:UserName\Documents\WindowsPowerShell\" -ItemType Directory
 Copy-Item -Path "$pwd\*" -Destination "C:\Users\$Env:UserName\Documents\WindowsPowerShell" -Recurse
 Remove-Item "C:\Users\$Env:UserName\Documents\WindowsPowerShell\Instruction.txt"
 Remove-Item "C:\Users\$Env:UserName\Documents\WindowsPowerShell\PowerShell.ps1"
