178a7f30483ba0ff68370fdaeb072e2b67d35ce2
imranh37 <hmimran.in@gmail.com>
2023-07-07 19:48:34 +0600

    automate terminal script update3

--- automate-terminal/powershell-config/PowerShell2.ps1
+++ automate-terminal/powershell-config/PowerShell2.ps1
@@ -2,7 +2,7 @@
 Install-Module PSReadLine -AllowPrerelease -Force
 
 # Copying Files
-# New-Item "C:\Users\$Env:UserName\Documents\WindowsPowerShell\" -ItemType Directory
+New-Item "C:\Users\$Env:UserName\Documents\WindowsPowerShell\themes" -ItemType Directory
 Copy-Item -Path "$pwd\themes\*" -Destination "C:\Users\$Env:UserName\Documents\WindowsPowerShell\themes\" -Recurse
 Copy-Item -Path "$pwd\Microsoft.PowerShell_profile.ps1" -Destination "C:\Users\$Env:UserName\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
 
